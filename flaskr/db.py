bc2c4f8e676d27ad3e707cdbb91e235c22fcd169
Andrey05319 <65806981+Andrey05319@users.noreply.github.com>
2022-12-25 17:32:19 +0600

    Up

--- flaskr/db.py
+++ flaskr/db.py
@@ -21,6 +21,7 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
+
 def init_db():
     db = get_db()
 
@@ -35,3 +36,6 @@ def init_db_command():
     click.echo('Initialized the database.')
 
 
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
